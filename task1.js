5a8f614a8c3a888a68f77c0f4892f752d42f17f7
Timothy Ojo <61148770+timothyojo@users.noreply.github.com>
2020-03-28 23:26:51 +0100

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,7 @@
+const name = 'ojo oghenekaro timothy';
+console.log(name);
+const courses = ['html', 'css', 'javascript', 'node.js'];
+console.log(courses);
+for (let i = 1; i <= 200; i++) {
+  if (i % 2 !== 1) console.log(i);
+}
